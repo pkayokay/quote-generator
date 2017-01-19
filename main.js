5d4695d03ecff382678a59708c343ab89a820726
Paul Kim <pkayokay@gmail.com>
2017-01-19 18:21:14 -0500

    quote and author on load

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+
 var quotes = [
   "Life is 10% what happens to you and 90% how you react to it.",
-  "Good, better, best. Never let it rest. 'Til your good is better and your better is best."
+  "Good, better, best. Never let it rest. 'Til your good is better and your better is best.",
+  "Knowing is not enough; we must apply. Willing is not enough; we must do."
 ]
 
-getElementById('displayQuote').innerHTML = "cool";
+
+var author = [
+  "Charles R. Swindoll",
+  "Tim Duncan",
+  "Johann W. von Goethe"
+]
+
+window.onload = newQuote();
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
   document.getElementById('displayQuote').innerHTML = quotes[randomNumber];
-}
+  document.getElementById('displayAuthor').innerHTML =  "-" + author[randomNumber];
+};
+
 
